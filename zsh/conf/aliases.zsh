90553f4c3b4827541734e1a28d45929eeeeb3296
mugiwara@loki <saratangajala@gmail.com>
2026-02-03 23:44:23 +0530

    updates aliases

--- zsh/conf/aliases.zsh
+++ zsh/conf/aliases.zsh
@@ -11,6 +11,7 @@
 ########################
 alias cat=bat
 alias ls=lsd
+alias cd=z
 
 # --------------------------------------------------------------------------
 # Program Aliases
@@ -19,9 +20,15 @@ alias vim=nvim
 
 # --------------------------------------------------------------------------
 # Config file aliases
-alias zrc="vim ~/dotfiles/zsh/.zshrc"
-alias hrc="vim ~/dotfiles/hypr/hyprland.conf"
+alias zrc="vim ~/dotfiles/zsh/.zshrc -c \"cd ~/dotfiles\""
+alias hrc="vim ~/dotfiles/hypr/hyprland.conf -c \"cd ~/dotfiles\""
 alias szrc="source ~/.zshrc"
 alias shrc="source ~/dotfiles/hypr/hyprland.conf"
-alias edit-alias="vim ~/dotfiles/zsh/conf/aliases.sh"
+alias edit-alias="vim ~/dotfiles/zsh/conf/aliases.zsh -c \"cd ~/dotfiles\""
+# --------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------
+# port forwarding
+alias pf-imdi-clickhouse="kubectl --kubeconfig ~/files/anera/infra/kubeconfig.yaml port-forward svc/clickhouse 8125:8123 -n prod-intelligence-markets-data-ingestion"
+alias pf-imdi-mongo="kubectl --kubeconfig ~/files/anera/infra/kubeconfig.yaml port-forward svc/mongodb-headless 27019:27017 -n prod-intelligence-markets-data-ingestion"
 # --------------------------------------------------------------------------
